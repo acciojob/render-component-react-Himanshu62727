d3c5f8b5e8a3213c54d76fba7bf21a9bb65d66c6
Himanshu Singh <124270542+Himanshu62727@users.noreply.github.com>
2025-02-14 19:44:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 
+before(() => {
+  cy.visit('http://localhost:8080'); // Ensure this URL is correct
+});
+
+it('should check if p tag is rendered', () => {
+  cy.get('p').should('contain.text', 'I am learning React. My life is getting better.');
+});
+
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
