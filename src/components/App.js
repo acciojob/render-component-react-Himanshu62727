e195fe9e9498f6857d755416214bdda7076c5ef4
Himanshu Singh <124270542+Himanshu62727@users.noreply.github.com>
2025-03-03 20:03:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React, {Component, useState} from "react";
+import '../styles/App.css';
 
-function App() {
-  return <p>I am learning React. My life is getting better.</p>;
+const App = () => {
+  return (
+    <div id="main">
+      <p>I am learning React. My life is getting better.</p>
+    </div>
+  )
 }
 
-const root = document.getElementById("root");
-createRoot(root).render(<App />);
+
+export default App;
