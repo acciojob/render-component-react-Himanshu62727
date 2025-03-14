b2f531dbf65f3b3478c0ac02ec39694cc4404562
Himanshu Singh <124270542+Himanshu62727@users.noreply.github.com>
2025-03-14 11:20:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,7 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
 
-const App = () => {
-  return (
-    <div id="main">
-      <p>I am learning React. My life is getting better.</p>
-    </div>
-  )
+function MyComponent() {
+  return <p>I am learning React. My life is getting better.</p>;
 }
 
-
-export default App;
+export default MyComponent;
